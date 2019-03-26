5404075b53c9fbe8eaed9332fa24fb553cb1f9a6
chydiwe <magiceco71@gmail.com>
2019-03-26 18:12:33 +0300

    del mob ver

--- .bemrc.js
+++ .bemrc.js
@@ -10,23 +10,18 @@ module.exports = {
       path: 'src/desktop',
       scheme: 'nested'
     },
-    {
-      layer: 'mobile',
-      path: 'src/mobile',
-      scheme: 'nested'
-    }
+
   ],
   // remove sets to build one universal bundle for index.html
   sets: {
-    desktop: 'common desktop',
-    mobile: 'common mobile'
+    desktop: 'common',
   },
   modules: {
     'bem-tools': {
       plugins: {
         create: {
           levels: {
-            'src/blocks': { default: true }
+            'src/blocks': {default: true}
           },
           techs: ['js', 'css'],
           templates: {
